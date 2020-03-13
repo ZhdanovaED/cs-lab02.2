8fa8c9a94b2b912140ddfa7bc180dac61fda1e77
Alice (ZhdanovaED) <alice@example.com>
2020-03-13 12:14:34 +0300

    code:max

--- main.cpp
+++ main.cpp
@@ -9,9 +9,20 @@ int main()
     cin>>a>>b;
 
     cout<<"A+B="<<a+b<<'\n'
-    <<"A-B="<<a-b<<'\n'
-    <<"A*B="<<a*b<<'\n'
-    <<"A/B="<<a/b<<'\n';
+        <<"A-B="<<a-b<<'\n'
+        <<"A*B="<<a*b<<'\n'
+        <<"A/B="<<a/b<<'\n';
+
+
+    if(a>b)
+    {
+        cout<<a;
+    }
+    else
+    {
+        cout<<b;
+    }
+
 
     return 0;
 }
